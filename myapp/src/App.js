3d92ea6a40a71c54425547e4ee63d1960176c8fa
Tech 360 <Terryagabi1@gmail.com>
2023-10-18 22:40:46 +0100

    add interests

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,14 +1,42 @@
-import React from "react"; 
 import './App.css';
-import Navbar from "./components/Navbar";
 
-function App() {
+const App = () =>{
   return (
-    <div className="App">
-      <Navbar/>      
+          <div className="App">
+            <h1>Hello React</h1>
+            This page will be based on my interest during my leisure time.
+            <p>
+            During my leisure time, i'm either:
+            At the Gym.
+            Learning coding online.
+            Watching Customised cars being driven.</p>
+
+            <h2>Car:</h2>
+            In the aspect of cars i'm a really big fan of the mustang gt500 shelby, 
+            the sound that is produced from the car while being driven is astonishing 
+            the Mustang GT500 is a muscle/sports car with the starting cost of over $72,900 price range,
+            the car transmission is replaced with a TREMEC 7-SPEED dual-clutch transmission and a 5.2L V8 
+            the engine of this car produces over 760 horsepower.
+            Here is a link with the infos about the Mustang GT500 shelby:
+
+            <a href="https://www.caranddriver.com/ford/mustang-shelby-gt500"> MUSTANG GT500</a>
+
+
+            <h2>Music:</h2>
+            I have a big interest with music, the flow and remedy in music is astonishing.
+            music in a way is a form of therapy, the beauty of sounds being mixed together to make a decent
+            melody which is amusing to the ears is breathe-taking.
+          
+              <p>here are a list of artist with a link to acquire more infomation about them:</p>
+
+              <a href="https://en.wikipedia.org/wiki/Lil_Baby">lil baby</a>
+                  <p>
+                  <a href="https://en.wikipedia.org/wiki/Lil_Durk">lil durk</a></p>
+              
+                
+                   Copyright (c) Terry Agabi 2021	
+      </div>)
   
-         </div>
-  );
-}
+};
 
 export default App;
